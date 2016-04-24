3065eed180f3fb81b3a163b1fe5c9fc0e93e7c44
Nicolas Alejandro Cancino Cancino <com16nico@gmail.com>
2016-04-24 16:09:32 -0300

    Terminada la reserva de horas

--- app/src/main/java/sampleapp/getmed/Main_Menu.java
+++ app/src/main/java/sampleapp/getmed/Main_Menu.java
@@ -1,18 +1,24 @@
 package sampleapp.getmed;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
+import android.os.AsyncTask;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class Main_Menu extends AppCompatActivity {
@@ -94,7 +100,8 @@ public class Main_Menu extends AppCompatActivity {
         int id = item.getItemId();
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-           //Do Shit
+
+            showEditDialog();
             return true;
         }
 
@@ -111,6 +118,50 @@ public class Main_Menu extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    private void showEditDialog() {
+        LayoutInflater li = LayoutInflater.from(Main_Menu.this);
+        View promptsView = li.inflate(R.layout.dialoglay, null);
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                Main_Menu.this);
+
+        // set prompts.xml to alertdialog builder
+        alertDialogBuilder.setView(promptsView);
+
+        final EditText userInput = (EditText) promptsView
+                .findViewById(R.id.editTextDialogUserInput);
+
+        // set dialog message
+        alertDialogBuilder
+                .setCancelable(false)
+                .setPositiveButton("OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                // get user input and set it to result
+                                // edit text
+                                SharedPreferences preferences = getSharedPreferences("Preferencia_usuario", Context.MODE_PRIVATE);
+                                String pref = preferences.getString("usuario","");
+
+                                new Asyncs().execute(pref,userInput.getText().toString(),null);
+                            }
+                        })
+                .setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // show it
+        alertDialog.show();
+
+    }
+
+
+
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
@@ -124,7 +175,85 @@ public class Main_Menu extends AppCompatActivity {
     }
 
 
+    private class Asyncs extends AsyncTask<String, String, String> {
 
 
+        @Override
+        protected String doInBackground(String... params) {
+            String usuario = params[0];
+              String   motivo = params[1];
+
+            if (usuario.equals("") ||motivo.equals("")) {
+                runOnUiThread(new Runnable() {
+
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), "Ingrese los datos",
+                                Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+            } else {
+                try {
+                    HttpHandler sh = new HttpHandler(getApplicationContext());
+                    String resu = sh.get("http://192.168.2.3/getmed/Agendar.php?user=" + usuario + "&motivo=" + motivo);
+
+
+                    switch (resu) {
+                        case "Todo ok":
+                            runOnUiThread(new Runnable() {
+
+                                public void run() {
+                                    Toast.makeText(getApplicationContext(), "Su consulta se ha agendado con exito",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+                            });
+
+
+                            //startActivity(new Intent(MainActivity.this, Main_Menu.class));//ir a perfil luego del login
+                            break;
+
+                        case "No consulta":
+                            runOnUiThread(new Runnable() {
+
+                                public void run() {
+                                    Toast.makeText(getApplicationContext(), "No quedan horas disponibles",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+                            });
+                            break;
+                        case "ERROR":
+                            runOnUiThread(new Runnable() {
+
+                                public void run() {
+                                    Toast.makeText(getApplicationContext(), "No se pudo conectar al servidor o no quedan horas",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+                            });
+                            break;
+                        default:
+                            //
+
+                    }
+                } catch (Exception e) {
+                    runOnUiThread(new Runnable() {
+
+                        public void run() {
+                            Toast.makeText(getApplicationContext(), "error", Toast.LENGTH_LONG).show();
+
+                        }
+                    });
+                }
+            }
+            return null;
+        }
+
+
+
+    }
 }
+
+
 
